5fa62f473566e26d6c6a4d8a2527046225cdf030
Shinnku <nikaidou@shinnku.com>
2025-07-15 19:35:50 +0800

    feat: add proxy route for serving files from S3

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
 import { Hono } from 'hono'
+import { proxy } from 'hono/proxy'
 import { renderer } from './renderer'
 
 const app = new Hono()
@@ -7,6 +8,12 @@ app.use(renderer)
 
 app.get('/', (c) => {
   return c.render(<h1>Hello!</h1>)
+})
+
+app.get('/file/shinnku/*', (c) => {
+  const slug = c.req.path.replace(/^\/file\/shinnku\//, '')
+  const target = `https://shinnku.s3.us-west-004.backblazeb2.com/${slug}`
+  return proxy(target)
 })
 
 export default app
